7f175058ad6c0d7ac24abd39ee55f789f17bac40
jokulyao <forlive_yxs@qq.com>
2015-10-27 11:50:45 +0800

    更新项目

--- Spring_1000_IOC_Annotation_Autowired_Qulifier/src/com/bjsxt/service/UserService.java
+++ Spring_1000_IOC_Annotation_Autowired_Qulifier/src/com/bjsxt/service/UserService.java
@@ -1,34 +1,34 @@
 package com.bjsxt.service;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.bjsxt.dao.UserDAO;
 import com.bjsxt.model.User;
 
-
-
 public class UserService {
-	
-	private UserDAO userDAO;  
-	
+
+	private UserDAO userDAO;
+
 	public void init() {
 		System.out.println("init");
 	}
-	
+
 	public void add(User user) {
 		userDAO.save(user);
 	}
+
 	public UserDAO getUserDAO() {
 		return userDAO;
 	}
-	
+
 	@Autowired
-	public void setUserDAO(@Qualifier("u") UserDAO userDAO) {
+	// 自动装配，首先按类型匹配，如果匹配成功，装入；如果类型匹配到多个，则报错
+	// 需要用@Qualifier指定特定的名字
+	public void setUserDAO(@Qualifier("userDAO") UserDAO userDAO) {
 		this.userDAO = userDAO;
 	}
-	
 
-	
 	public void destroy() {
 		System.out.println("destroy");
 	}
