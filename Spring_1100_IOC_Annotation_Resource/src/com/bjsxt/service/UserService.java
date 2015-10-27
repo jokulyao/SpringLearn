9a2e787565b43d13f7833dc1caca0f33da908171
jokulyao <forlive_yxs@qq.com>
2015-10-27 11:56:52 +0800

    更新项目

--- Spring_1100_IOC_Annotation_Resource/src/com/bjsxt/service/UserService.java
+++ Spring_1100_IOC_Annotation_Resource/src/com/bjsxt/service/UserService.java
@@ -1,4 +1,5 @@
 package com.bjsxt.service;
+
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,30 +8,28 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import com.bjsxt.dao.UserDAO;
 import com.bjsxt.model.User;
 
-
-
 public class UserService {
-	
-	private UserDAO userDAO;  
-	
+
+	private UserDAO userDAO;
+
 	public void init() {
 		System.out.println("init");
 	}
-	
+
 	public void add(User user) {
 		userDAO.save(user);
 	}
+
 	public UserDAO getUserDAO() {
 		return userDAO;
 	}
-	
+
 	@Resource
-	public void setUserDAO( UserDAO userDAO) {
+	//注意JDK1.5没有@Resource注解，@Resource从JDK1.6开始出现的
+	public void setUserDAO(UserDAO userDAO) {
 		this.userDAO = userDAO;
 	}
-	
 
-	
 	public void destroy() {
 		System.out.println("destroy");
 	}
