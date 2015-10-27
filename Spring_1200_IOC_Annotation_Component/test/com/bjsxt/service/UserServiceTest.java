7d0ce81a3274ebbf988a835b42b110c764df8818
jokulyao <forlive_yxs@qq.com>
2015-10-27 12:00:36 +0800

    更新项目

--- Spring_1200_IOC_Annotation_Component/test/com/bjsxt/service/UserServiceTest.java
+++ Spring_1200_IOC_Annotation_Component/test/com/bjsxt/service/UserServiceTest.java
@@ -1,4 +1,5 @@
 package com.bjsxt.service;
+
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,15 +10,13 @@ import com.bjsxt.model.User;
 //Inverse of Control
 public class UserServiceTest {
 
-	@Test 
+	@Test
 	public void testAdd() throws Exception {
-		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
-		
-		UserService service = (UserService)ctx.getBean("userService");
-		service.add(new User());
-		
-		ctx.destroy();
-		
-	}
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"beans.xml");
 
+		UserService service = (UserService) ctx.getBean("userService");
+		service.add(new User());
+		ctx.destroy();
+	}
 }
