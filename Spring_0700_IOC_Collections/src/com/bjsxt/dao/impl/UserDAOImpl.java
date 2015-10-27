322cc7a25153adae900f9f35206c2aa5b4fd1f35
jokulyao <forlive_yxs@qq.com>
2015-10-27 11:14:04 +0800

    更新项目

--- Spring_0700_IOC_Collections/src/com/bjsxt/dao/impl/UserDAOImpl.java
+++ Spring_0700_IOC_Collections/src/com/bjsxt/dao/impl/UserDAOImpl.java
@@ -15,47 +15,32 @@ public class UserDAOImpl implements UserDAO {
 	private List<String> lists;
 	private Map<String , String> maps;
 	
-	
 	public Set<String> getSets() {
 		return sets;
 	}
-
-
 
 	public void setSets(Set<String> sets) {
 		this.sets = sets;
 	}
 
-
-
 	public List<String> getLists() {
 		return lists;
 	}
-
-
 
 	public void setLists(List<String> lists) {
 		this.lists = lists;
 	}
 
-
-
 	public Map<String, String> getMaps() {
 		return maps;
 	}
-
-
 
 	public void setMaps(Map<String, String> maps) {
 		this.maps = maps;
 	}
 
-
-
 	public UserDAOImpl() {
 	}
-	
-	
 
 	public void save(User user) {
 		System.out.println("user saved!");
@@ -65,5 +50,4 @@ public class UserDAOImpl implements UserDAO {
 	public String toString() {
 		return "sets size:" + sets.size() + "| lists size:" + lists.size() + "| maps size:" + maps.size() ; 
 	}
-
 }
