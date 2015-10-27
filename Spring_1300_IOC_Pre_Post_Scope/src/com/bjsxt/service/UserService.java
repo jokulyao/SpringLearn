6f8ca2375eb8ababfb09f394edad4c0a82bd96bc
jokulyao <forlive_yxs@qq.com>
2015-10-27 12:58:03 +0800

    更新项目

--- Spring_1300_IOC_Pre_Post_Scope/src/com/bjsxt/service/UserService.java
+++ Spring_1300_IOC_Pre_Post_Scope/src/com/bjsxt/service/UserService.java
@@ -1,4 +1,5 @@
 package com.bjsxt.service;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
@@ -10,29 +11,28 @@ import org.springframework.stereotype.Component;
 import com.bjsxt.dao.UserDAO;
 import com.bjsxt.model.User;
 
-
 @Component("userService")
 public class UserService {
-	
-	private UserDAO userDAO;  
-	
+
+	private UserDAO userDAO;
+
 	@PostConstruct
 	public void init() {
 		System.out.println("init");
 	}
-	
+
 	public void add(User user) {
 		userDAO.save(user);
 	}
+
 	public UserDAO getUserDAO() {
 		return userDAO;
 	}
-	
-	@Resource(name="u")
-	public void setUserDAO( UserDAO userDAO) {
+
+	@Resource(name = "u")
+	public void setUserDAO(UserDAO userDAO) {
 		this.userDAO = userDAO;
 	}
-	
 
 	@PreDestroy
 	public void destroy() {
